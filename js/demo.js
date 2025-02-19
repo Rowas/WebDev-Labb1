0b78f0f17811c81505e58dc2556d7d12dec11232
Andreas Lind-Sahlin <Rowas@users.noreply.github.com>
2025-02-20 00:36:57 +0100

    Final Push
    Final changes.
    Final Lighthouse tests done and printscreened for turn in.

--- js/demo.js
+++ js/demo.js
@@ -37,49 +37,49 @@ if (document.title == "Cart")
     alert("Not Impemented.")
   })
   
-  let cartItem = 
-  `
-                            <div class="card rounded-3 mb-4">
-                              <div class="card-body p-4">
-                                <div class="row d-flex justify-content-between align-items-center">
-                                  <div class="col-md-2 col-lg-2 col-xl-2">
-                                    <img fetchpriority="high"
-                                      src="./media/${prod4.prodImg}" height="158" width="126"
-                                      class="img-fluid rounded-3" alt="${prod4.prodName}">
-                                  </div>
-                                  <div class="col-md-3 col-lg-3 col-xl-3">
-                                    <p class="lead fw-normal mb-2">${prod4.prodName}</p>
-                                  </div>
-                                  <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                    <button aria-label="Less Items" data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                      onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                      <i class="fas fa-minus"></i>
-                                    </button>
+  // let cartItem = 
+  // `
+  //                           <div class="card mb-4">
+  //                             <div class="card-body p-4">
+  //                               <div class="row d-flex justify-content-between align-items-center">
+  //                                 <div class="col-md-2 col-lg-2 col-xl-2">
+  //                                   <img fetchpriority="high"
+  //                                     src="./media/${prod4.prodImg}" height="158" width="126"
+  //                                     class="img-fluid" alt="${prod4.prodName}">
+  //                                 </div>
+  //                                 <div class="col-md-3 col-lg-3 col-xl-3">
+  //                                   <p class="lead fw-normal mb-2">${prod4.prodName}</p>
+  //                                 </div>
+  //                                 <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+  //                                   <button aria-label="Less Items" data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+  //                                     onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+  //                                     <i class="fas fa-minus"></i>
+  //                                   </button>
                     
-                                    <input aria-label="quantity" id="quantity" min="0" name="quantity" value="1" type="number"
-                                      class="form-control form-control-sm" />
+  //                                   <input aria-label="quantity" id="quantity" min="0" name="quantity" value="1" type="number"
+  //                                     class="form-control form-control-sm" />
                     
-                                    <button aria-label="More Items" data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                      onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                      <i class="fas fa-plus"></i>
-                                    </button>
-                                  </div>
-                                  <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                    <h5 class="mb-0">${prod4.prodPrice} SEK</h5>
-                                  </div>
-                                  <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                    <a aria-label="remove item" href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-  `
+  //                                   <button aria-label="More Items" data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
+  //                                     onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+  //                                     <i class="fas fa-plus"></i>
+  //                                   </button>
+  //                                 </div>
+  //                                 <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+  //                                   <h5 class="mb-0">${prod4.prodPrice} SEK</h5>
+  //                                 </div>
+  //                                 <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+  //                                   <a aria-label="remove item" href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+  //                                 </div>
+  //                               </div>
+  //                             </div>
+  //                           </div>
+  // `
   
-  let cartItemArray = []
+  // let cartItemArray = []
   
-  cartItemArray.push(cartItem);
+  // cartItemArray.push(cartItem);
   
-  document.getElementById("cartItems").insertAdjacentHTML("afterbegin", cartItemArray)
+  // document.getElementById("cartItems").insertAdjacentHTML("afterbegin", cartItemArray)
   }
 
 if (document.title == "Produkter")
@@ -186,6 +186,7 @@ AddCartBtn.addEventListener("click", () => {
 })
 }
 }
+
 const navigation = `
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #f0ead6;">
   <div class="container-fluid">
